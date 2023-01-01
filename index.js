3fb76990adfb6f7549e702b9beebe4540030e4c9
yashpurkar <yashpurkar7079@gmail.com>
2023-01-01 12:03:10 +0530

    got data from server

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ let formElem = document.querySelector("#form");
 let inputText = document.querySelector("#inputText");
 
 
-const showResult = (searchValue) => {
+const showResult = async (searchValue) => {
   console.log(searchValue);
+
+  let wikipediaUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="instagram"`
+  const response = await fetch(wikipediaUrl);
+  const data = await response.json();
+  console.log(data);
 }
 
 formElem.addEventListener("submit", function (event) {
@@ -11,3 +16,6 @@ formElem.addEventListener("submit", function (event) {
   // console.log("Working");
   showResult(inputText.value);
 })
+
+
+// https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="USER_INPUT_SEARCH_TERM".
